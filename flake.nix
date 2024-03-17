1fda5ecd897e50c915ef36a2abfb69145ddf89be
meersehn <>
2024-03-17 19:13:03 +0100

    added flake.nix

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    ...
+  }:
+  let 
+    system = "x86_64-linux";
+    pkgs = import nixpkgs { inherit system; };
+  in {
+    devShell."${system}" = pkgs.mkShell rec {
+      packages = with pkgs; [
+        rlwrap
+        sbcl.withPackages (ps: with ps; [
+          quickproject
+        ])
+      ];
+    };
+  };
+}
